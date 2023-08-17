45b5cd024167776d38c592a2bbdb852c52520b16
bresonjr <79446230+bresonjr@users.noreply.github.com>
2023-08-17 18:53:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '',
-        pass: '',
+        user: 'bresonjraj10@gmail.com',
+        pass: 'viunnwoaivzbxfco',
     },
     secure: true,
 });
